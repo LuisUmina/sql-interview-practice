ed6c4ef2dcd00e10312f2a22caafdb4e1437708e
Luis Angel Umiña Navia <luis-navia-2002@hotmail.com>
2026-01-08 00:44:48 -0500

    Add DataLemur Easy: Histogram of Tweets

--- datalemur/easy/twitter__histogram_of_tweets.sql
+++ datalemur/easy/twitter__histogram_of_tweets.sql
@@ -3,6 +3,8 @@
 -- Key idea: count tweets per user in 2022, then histogram by that count
 -- Link: https://datalemur.com/questions/sql-histogram-tweets
 
+
+-- Solution 1: CTE
 WITH cte AS (
   SELECT user_id, COUNT(tweet_id) AS tweets_num
   FROM tweets
@@ -12,3 +14,14 @@ WITH cte AS (
 SELECT tweets_num AS tweet_bucket, COUNT(user_id) AS users_num
 FROM cte
 GROUP BY tweets_num
+
+
+-- Solution 2: Subquery
+SELECT tweets_num AS tweet_bucket, COUNT(user_id) AS users_num
+FROM (
+  SELECT user_id, COUNT(tweet_id) AS tweets_num
+  FROM tweets
+  WHERE tweet_date BETWEEN '2022-01-01' AND '2022-12-31'
+  GROUP BY user_id
+) t
+GROUP BY tweets_num;
